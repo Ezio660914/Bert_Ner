34c1df4691e177148d90b67fc2e4e088f3099b5a
Ezio660914 <424176345@qq.com>
2021-09-07 21:33:31 +0800

    one hot 编码标签

--- PreprocessData.py
+++ PreprocessData.py
@@ -8,41 +8,77 @@
 """
 from pathlib import Path
 import pickle
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
 
+tf.get_logger().setLevel("ERROR")
 dataDir = Path("./downloads/Resource/CoNLL")
 rawTrainFile = "conllpp_train.txt"
 rawValFile = "conllpp_dev.txt"
 rawTestFile = "conllpp_test.txt"
 
 
+def GetClassNames(fileDir):
+    classNames = []
+    with open(fileDir, "r", encoding="utf8") as f:
+        for line in f:
+            if not line == "\n":
+                wordList = line.split(" ")
+                cls = wordList[-1].replace("\n", "")
+                if cls not in classNames:
+                    classNames.append(cls)
+    classNames.sort()
+    print(classNames)
+    return classNames
+
+
+def GetOneHotEncoder():
+    classNames = GetClassNames(dataDir / rawTrainFile)
+    classNames = np.array(classNames)[:, None]
+    encoder = OneHotEncoder(sparse=False, dtype=np.float32)
+    encoder.fit(classNames)
+    return encoder
+
+
 def PreProcessFile(fileDir):
+    encoder = GetOneHotEncoder()
+    sentenceList = []
+    labelList = []
+    sentence = ""
+    label = []
+    classNames = []
     with open(fileDir, "r", encoding="utf-8") as f:
-        sentenceList = []
-        labelList = []
-        sentence = ""
-        label = []
         for line in f:
             if not line == "\n":
                 wordList = line.split(" ")
                 sentence = sentence + wordList[0] + " "
-                label.append(wordList[-1].replace("\n", ""))
+                className = wordList[-1].replace("\n", "")
+                label.append([className])
+                if className not in classNames:
+                    classNames.append(className)
+
             else:
                 sentenceList.append(sentence.rstrip())
-                labelList.append(label)
+                oneHotLabel = encoder.transform(label).tolist()
+                labelList.append(oneHotLabel)
                 sentence = ""
                 label = []
-    return sentenceList, labelList
+    labelList = tf.ragged.constant(labelList)
+    return sentenceList, labelList, classNames
 
 
 def main():
-    sentenceList, labelList = PreProcessFile(dataDir / rawTrainFile)
-    # print(sentenceList[:10])
-    # print(labelList)
+    sentenceList, labelList, classNames = PreProcessFile(dataDir / rawTrainFile)
+    print(sentenceList[:3])
+    print(labelList)
     with open("sentenceList.pkl", "wb") as f:
         pickle.dump(sentenceList, f)
     with open("labelList.pkl", "wb") as f:
         pickle.dump(labelList, f)
-    exit(0)
+    with open("classNames.pkl", "wb") as f:
+        pickle.dump(classNames, f)
 
 
 if __name__ == "__main__":
